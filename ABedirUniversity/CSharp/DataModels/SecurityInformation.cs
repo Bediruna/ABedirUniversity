62977147cf446e522d604d04b2e43836e3cb53f4
Bedir <Bediruna@hotmail.com>
2020-05-18 19:46:38 -0400

    minor changes

--- ABedirUniversity/CSharp/DataModels/SecurityInformation.cs
+++ ABedirUniversity/CSharp/DataModels/SecurityInformation.cs
@@ -1,9 +1,16 @@
-﻿namespace ABedirUniversity.CSharp
+﻿using System;
+
+namespace ABedirUniversity.CSharp
 {
     public class SecurityInformation
     {
-        public string Status { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
         public string HashedPassword { get; set; }
         public string PasswordSalt { get; set; }
+        public string IPAddress { get; set; }
+        public string Status { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public DateTime UserCreateDate { get; set; }
     }
 }
