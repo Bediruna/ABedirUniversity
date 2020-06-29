6efedd809d0304ab0a410997e7a18963e7cac862
Bedir <Bediruna@hotmail.com>
2020-06-29 04:24:33 -0400

    Added unit test

---
+++ ClassLibrary/StudentApplicationsTests.cs
@@ -0,0 +1,22 @@
+﻿using ABedirUniversity.CSharp;
+using ABedirUniversity.CSharp.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ClassLibrary
+{
+    public class StudentApplicationsTests
+    {
+        [Fact]
+        public void ValidateSecurityInfo_ShouldValidate()
+        {
+            SecurityInformation expectedSI = new SecurityInformation();
+
+            SecurityInformation actualSI = Validator.ValidateSecurityInfoInput("TestUserName", "testpassword", "pending", "student");
+
+            Assert.Equal(expectedSI, actualSI);
+        }
+    }
+}
---
+++ TestLibrary/PasswordTests.cs
@@ -0,0 +1,18 @@
+﻿using ABedirUniversity.CSharp;
+using Xunit;
+
+namespace TestLibrary
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void ShouldValidateSecurity()
+        {
+            string plaintextshouldnotequal = "testpasswordtestsalt";
+
+            string actual = PasswordManager.HashPassword("testpassword", "testsalt");
+
+            Assert.NotEqual(plaintextshouldnotequal, actual);
+        }
+    }
+}
